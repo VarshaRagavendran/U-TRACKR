b9c5d6ec1fceed3cae6bd9b0d22b28d5c1ae39f7
VarshaRagavendran <VarshaRagavendran@users.noreply.github.com>
2018-01-28 16:46:12 -0500

    code cleanup & looping over tracked points
    Looping over tracked points to show object movement.

--- beta/main.py
+++ beta/main.py
@@ -5,16 +5,20 @@ import imutils
 
 
 cap = cv2.VideoCapture();
-cap.open('tcp://192.168.0.37:5000')
+#IP addr to change depending on Pi
+cap.open('tcp://192.168.86.133:5000')
 
 
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret:
-    	# define the lower and upper boundaries of the "green"
+
+        frame = imutils.resize(frame, width=400)
+
+        # define the lower and upper boundaries of the "green"
         # ball in the HSV color space
-        greenLower = (29, 86, 6)
-        greenUpper = (64, 255, 255)
+        greenLower = np.array([29, 86, 6])
+        greenUpper = np.array([64, 255, 255])
 
         # initialize the list of tracked points, the frame counter,
         # and the coordinate deltas
@@ -60,11 +64,20 @@ while(cap.isOpened()):
                 cv2.circle(frame, (int(x), int(y)), int(radius),
                     (0, 255, 255), 2)
                 cv2.circle(frame, center, 5, (0, 0, 255), -1)
-                pts.appendleft(center)
-            for i in np.arange(1, len(pts)):
-                thickness = int(np.sqrt(32 / float(i + 1)) * 2.5)
-                cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+        pts.appendleft(center)
 
+        # loop over the set of tracked points
+        for i in xrange(1, len(pts)):
+            # if either of the tracked points are None, ignore
+            # them
+            if pts[i - 1] is None or pts[i] is None:
+                continue
+ 
+            # otherwise, compute the thickness of the line and
+            # draw the connecting lines
+            thickness = int(np.sqrt(32 / float(i + 1)) * 2.5)
+            cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+   
             # show the movement deltas and the direction of movement on
             # the frame
             #cv2.putText(frame, "x: {}, y: {}, radius: {}".format(x, y, radius),
@@ -75,12 +88,11 @@ while(cap.isOpened()):
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
 
-		# if the 'q' key is pressed, stop the loop
-	if key == ord("q"):
-		break
-		
+        # if the 'q' key is pressed, stop the loop
+    if key == ord("q"):
+        break
+        
 # Release everything if job is finished
 cap.release()
 cv2.destroyAllWindows()
  
-
