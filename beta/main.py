521246e0dac8100a16ea72dccc818e9d1e5637a0
Kevin Arindaeng <karindaeng@gmail.com>
2018-02-01 21:02:15 -0500

    run tracker and timestamp method simultaneously

--- beta/main.py
+++ beta/main.py
@@ -1,20 +1,19 @@
-from multiprocessing import Process
+import threading
+from threading import Thread
 from tracker import tracker
 
-utrackr = tracker('192.168.0.39')
-#utrackr.hsvModifier()
-utrackr.tracker()
+utrackr = tracker('192.168.0.33')
+# utrackr.hsvModifier()
+# utrackr.tracker()
 
-# def func1():
-# 	utrackr.printTimeStamp()
+def func1():
+	utrackr.printTimeStamp()
 
-# def func2():
-# 	utrackr.tracker()
+def func2():
+	utrackr.tracker()
 
-# if __name__=='__main__':
-# 	p1 = Process(target = func1)
-# 	p1.start()
-# 	p2 = Process(target = func2)
-# 	p2.start()
+if __name__=='__main__':
+	Thread(target = func1).start()
+	Thread(target = func2).start()
 
 # utrackr.stopTracker()
