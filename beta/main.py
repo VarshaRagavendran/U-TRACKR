0b00610777de594be0a5f47d0c5316f4026b9b89
Kevin Arindaeng <karindaeng@gmail.com>
2018-02-02 19:49:14 -0500

    can run multiple pi cameras in multiple threads

--- beta/main.py
+++ beta/main.py
@@ -1,19 +1,25 @@
-import threading
 from threading import Thread
 from tracker import tracker
 
 utrackr = tracker('192.168.0.33')
-# utrackr.hsvModifier()
-# utrackr.tracker()
+utrackr2 = tracker('192.168.0.39')
 
 def func1():
-	utrackr.printTimeStamp()
+	while (utrackr.cap.isOpened()):
+		print "cam1: " + utrackr.timesync.getTimeStamp()
 
 def func2():
-	utrackr.tracker()
+	utrackr.outputFrame("frame1")
+
+def func3():
+	while (utrackr2.cap.isOpened()):
+		print "cam2: " + utrackr2.timesync.getTimeStamp()
+
+def func4():
+	utrackr2.outputFrame("frame2")
 
 if __name__=='__main__':
 	Thread(target = func1).start()
 	Thread(target = func2).start()
-
-# utrackr.stopTracker()
+	Thread(target = func3).start()
+	Thread(target = func4).start()
--- beta/tracker.py
+++ beta/tracker.py
@@ -16,12 +16,11 @@ class tracker(object):
         while(self.cap.isOpened()):
             print self.timesync.getTimeStamp()
 
-    def tracker(self):
-        while(self.cap.isOpened()):
+    def getFrame(self):
+        frame = np.zeros(shape=(600, 400, 3)).astype('uint8')
+        if (self.cap.isOpened()):
             ret, frame = self.cap.read()
             if ret:
-                frame = imutils.resize(frame, width=400)
-
                 # define the lower and upper boundaries of the "green"
                 # ball in the HSV color space
                 greenLower = np.array([29, 86, 6])
@@ -30,11 +29,83 @@ class tracker(object):
                 # initialize the list of tracked points, the frame counter,
                 # and the coordinate deltas
                 pts = deque(maxlen=32)
-                counter = 0
-                (dX, dY) = (0, 0)
-                direction = ""
+                # counter = 0
+                # (dX, dY) = (0, 0)
+                # direction = ""
 
-                (grabbed, frame) = self.cap.read()
+                # (grabbed, frame) = self.cap.read()
+                # resize the frame, blur it, and convert it to the HSV
+                # color space
+                frame = imutils.resize(frame, width=600)
+                # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+                hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+                # construct a mask for the color "green", then perform
+                # a series of dilations and erosions to remove any small
+                # blobs left in the mask
+                mask = cv2.inRange(hsv, greenLower, greenUpper)
+                mask = cv2.erode(mask, None, iterations=2)
+                mask = cv2.dilate(mask, None, iterations=2)
+
+                # find contours in the mask and initialize the current
+                # (x, y) center of the ball
+                cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                        cv2.CHAIN_APPROX_SIMPLE)[-2]
+                center = None
+
+                # only proceed if at least one contour was found
+                if len(cnts) > 0:
+                    # find the largest contour in the mask, then use
+                    # it to compute the minimum enclosing circle and
+                    # centroid
+                    c = max(cnts, key=cv2.contourArea)
+                    ((x, y), radius) = cv2.minEnclosingCircle(c)
+                    M = cv2.moments(c)
+                    center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+                    # only proceed if the radius meets a minimum size
+                    if radius > 10:
+                        # draw the circle and centroid on the frame,
+                        # then update the list of tracked points
+                        cv2.circle(frame, (int(x), int(y)), int(radius),
+                                   (0, 255, 255), 2)
+                        cv2.circle(frame, center, 5, (0, 0, 255), -1)
+                pts.appendleft(center)
+
+                # loop over the set of tracked points
+                for i in xrange(1, len(pts)):
+                    # if either of the tracked points are None, ignore
+                    # them
+                    if pts[i - 1] is None or pts[i] is None:
+                        continue
+
+                    # otherwise, compute the thickness of the line and
+                    # draw the connecting lines
+                    thickness = int(np.sqrt(32 / float(i + 1)) * 2.5)
+                    cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+        return frame
+
+    def outputFrame(self, frameName):
+        while (self.cap.isOpened()):
+            cv2.imshow(frameName, self.getFrame())
+            key = cv2.waitKey(1) & 0xFF
+
+    def tracker(self):
+        while(self.cap.isOpened()):
+            ret, frame = self.cap.read()
+            if ret:
+                # define the lower and upper boundaries of the "green"
+                # ball in the HSV color space
+                greenLower = np.array([29, 86, 6])
+                greenUpper = np.array([64, 255, 255])
+
+                # initialize the list of tracked points, the frame counter,
+                # and the coordinate deltas
+                pts = deque(maxlen=32)
+                # counter = 0
+                # (dX, dY) = (0, 0)
+                # direction = ""
+
                 # resize the frame, blur it, and convert it to the HSV
                 # color space
                 frame = imutils.resize(frame, width=600)
