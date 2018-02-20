c62649775b4d3a6b232523746c2bf9077a3e121a
VarshaRagavendran <VarshaRagavendran@users.noreply.github.com>
2018-02-20 12:53:02 -0500

    update to wifi test case

--- beta/testing.py
+++ beta/testing.py
@@ -7,7 +7,20 @@ from checker import checker
 class TestUTrackr(unittest.TestCase):
 
     #FUNC-01
-    #def testWifiConnection(self):
+    def testWifiConnection(self):
+        #init of timesync will establish sshconnection
+        timesync2 = timesync('192.168.0.18')
+        timesync4 = timesync('192.168.0.19')
+        timesync3 = timesync('192.168.0.20')
+        timesync1 = timesync('192.168.0.21')
+        stdin1, stdout1, stderr1 = timesync1.ssh.exec_command('iwgetid -r')
+        stdin2, stdout2, stderr2 = timesync2.ssh.exec_command('iwgetid -r')
+        stdin3, stdout3, stderr3 = timesync3.ssh.exec_command('iwgetid -r')
+        stdin4, stdout4, stderr4 = timesync4.ssh.exec_command('iwgetid -r')
+        self.assertEquals(stdout1.read().rstrip(), stdout2.read().rstrip())
+        self.assertEquals(stdout2.read().rstrip(), stdout3.read().rstrip())
+        self.assertEquals(stdout3.read().rstrip(), stdout4.read().rstrip())
+        #todo: check with computer's wifi
 
     #FUNC-02
     def testSSHConnection(self):
