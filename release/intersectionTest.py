5ce3b4a2dfdf64636b0ac2995cc767148fda7c1e
Kevin Arindaeng <karindaeng@gmail.com>
2018-04-25 17:07:33 -0400

    remove intersectionTest

--- release/intersectionTest.py
+++
@@ -1,6 +0,0 @@
-from intersection import intersection
-
-#10 = x, 20 = y from each camera
-poscalc = intersection(10, 20, 10, 20, 10, 20, 10, 20)
-x, y, z = poscalc.position_calculation()
-print " x: " + str(x) + " y: " + str(y) + " z: " + str(z)
